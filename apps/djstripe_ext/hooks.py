c28994b65c7edc9a2c8af07cf30483b6ac8f3f12
David Burke <david@burkesoftware.com>
2024-11-08 15:53:22 -0500

    Add stripe webhook handler to check throttle

---
+++ apps/djstripe_ext/hooks.py
@@ -0,0 +1,12 @@
+from djstripe import webhooks
+
+print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+
+@webhooks.handler("customer.subscription.updated")
+def update_subscription(event, **kwargs):
+    """When the subscription is updated, immediately check for throttle adjustments"""
+    # Avoid importing models during django app startup
+    from apps.organizations_ext.tasks import check_organization_throttle
+
+    check_organization_throttle.delay(event.customer.subscriber_id)
