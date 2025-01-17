3714c50393973daafba599acfc92a683e20474d6
David Burke <david@burkesoftware.com>
2025-01-16 21:02:44 -0500

    Partial base href support

--- glitchtip/views.py
+++ glitchtip/views.py
@@ -1,5 +1,11 @@
+from django.conf import settings
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 async def health(request):
     return HttpResponse("ok", content_type="text/plain")
+
+
+def index(request, *args):
+    return render(request, "index.html", {"base_path": settings.FORCE_SCRIPT_NAME})
