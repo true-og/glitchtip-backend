40c2b97636053831594a7d33de7b68b82e6b1232
James Kiger <james.kiger@gmail.com>
2025-02-05 13:56:25 -0500

    Fix stripe webhook hook setup

--- apps/djstripe_ext/hooks.py
+++ apps/djstripe_ext/hooks.py
@@ -1,10 +1,10 @@
 from djstripe.event_handlers import djstripe_receiver
 
-djstripe_receiver(["customer.subscription.updated", "customer.subscription.created"])
+
+@djstripe_receiver(["customer.subscription.updated", "customer.subscription.created"])
 def update_subscription(event, **kwargs):
     """When the subscription is updated, immediately check for throttle adjustments"""
     # Avoid importing models during django app startup
     from apps.organizations_ext.tasks import check_organization_throttle
-
     if event.customer.subscriber_id:
         check_organization_throttle.delay(event.customer.subscriber_id)
