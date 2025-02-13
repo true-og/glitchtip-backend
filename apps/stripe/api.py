5f24d5ff2ee4a5e1f49dba130072a1c7451e7e82
David Burke <david@burkesoftware.com>
2025-02-12 20:18:46 -0500

    Add stripe get_subscription

--- apps/stripe/api.py
+++ apps/stripe/api.py
@@ -3,7 +3,7 @@ from ninja import ModelSchema, Router
 from glitchtip.api.authentication import AuthHttpRequest
 from glitchtip.schema import CamelSchema
 
-from .models import StripeProduct
+from .models import StripeProduct, StripeSubscription
 
 router = Router()
 
@@ -14,9 +14,31 @@ class StripeProductSchema(CamelSchema, ModelSchema):
         fields = ["stripe_id", "name", "description", "price", "events"]
 
 
+class StripeSubscriptionSchema(CamelSchema, ModelSchema):
+    class Meta:
+        model = StripeSubscription
+        fields = ["stripe_id", "created", "current_period_start", "current_period_end"]
+
+
 @router.get("products/", response=list[StripeProductSchema])
 async def list_products(request: AuthHttpRequest):
     return [
         product
         async for product in StripeProduct.objects.filter(is_public=True, events__gt=0)
     ]
+
+
+@router.get(
+    "subscriptions/{slug:organization_slug}/", response=StripeSubscriptionSchema | None
+)
+async def get_subscription(request: AuthHttpRequest, organization_slug: str):
+    return await (
+        StripeSubscription.objects.filter(
+            organization__users=request.auth.user_id,
+            organization__slug=organization_slug,
+            is_active=True,
+        )
+        .select_related("product")
+        .order_by("-created")
+        .afirst()
+    )
