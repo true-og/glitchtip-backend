b0b34e277d5af13ff8c95c89f2a0297a2e91e59b
david <david@burkesoftware.com>
2025-03-09 14:25:36 -0400

    Fix bug in social organization signal

--- apps/users/apps.py
+++ apps/users/apps.py
@@ -4,5 +4,5 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     name = "apps.users"
 
-    # def ready(self):
-    # import apps.users.signals  # noqa: F401 #This import registers the listener
+    def ready(self):
+        import apps.users.signals  # noqa: F401 #This import registers the listener
