641e58b8a95e425049227ef9f094b95fc98a6464
Trystan Mata <trystan.mata@evoludata.com>
2025-11-03 15:15:51 +0100

    fix: Check for provider ID in social organization signal

--- apps/users/tests/test_signals.py
+++ apps/users/tests/test_signals.py
@@ -14,9 +14,16 @@ class TestAddUserToSocialAppOragnizationTestCase(TestCase):
     def setUpTestData(cls):
         cls.organization_1 = baker.make("organizations_ext.Organization")
         cls.organization_2 = baker.make("organizations_ext.Organization")
+        cls.organization_3 = baker.make("organizations_ext.Organization")
         cls.user = baker.make("users.user")
         social_app_1 = baker.make(SocialApp, provider="google")
-        social_app_2 = baker.make(SocialApp, provider="facebook")
+        social_app_2 = baker.make(SocialApp, provider="facebook", provider_id="fb")
+        social_app_3 = baker.make(
+            SocialApp,
+            provider="openid_connect",
+            provider_id="my-openid",
+            settings={"server_url": "https://example.com"},
+        )
         baker.make(
             "organizations_ext.OrganizationSocialApp",
             organization=cls.organization_1,
@@ -27,21 +34,28 @@ class TestAddUserToSocialAppOragnizationTestCase(TestCase):
             organization=cls.organization_2,
             social_app=social_app_2,
         )
+        baker.make(
+            "organizations_ext.OrganizationSocialApp",
+            organization=cls.organization_3,
+            social_app=social_app_3,
+        )
         baker.make(SocialAccount, user=cls.user, provider="google")
-        baker.make(SocialAccount, user=cls.user, provider="facebook")
+        baker.make(SocialAccount, user=cls.user, provider="fb")
+        baker.make(SocialAccount, user=cls.user, provider="my-openid")
 
         cls.request = RequestFactory().get("/")
 
     def test_user_is_added_to_all_orgs_associated_to_their_social_apps(self):
-        with self.assertNumQueries(8):
+        with self.assertNumQueries(11):
             user_logged_in.send(sender=User, request=self.request, user=self.user)
         assert self.user in self.organization_1.users.all()
         assert self.user in self.organization_2.users.all()
+        assert self.user in self.organization_3.users.all()
 
     def test_user_is_not_added_to_orgs_not_associated_to_their_social_apps(self):
-        organization_3 = baker.make("organizations_ext.Organization")
+        organization_4 = baker.make("organizations_ext.Organization")
         user_logged_in.send(sender=User, request=self.request, user=self.user)
-        assert self.user not in organization_3.users.all()
+        assert self.user not in organization_4.users.all()
 
     def test_user_is_added_to_populated_orgs_associated_to_their_social_apps(self):
         user2 = baker.make("users.user")
